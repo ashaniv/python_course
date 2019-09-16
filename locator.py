fef88b7edd487c18c150e29d21ec465a28be8190
@ashaniv <alon.shin81@gmail.com>
2019-09-16 13:50:10 +0300

    after adding words count

--- locator.py
+++ locator.py
@@ -4,4 +4,19 @@ import requests
 url = input("Please enter a valid URL:")
 
 response = requests.get(url)
-print(response.text)
+#print(response.text)
+
+text = open(response.text)
+d = dict()
+
+for line in text:
+    line = line.strip()
+    line - line.lower()
+    words = line.split(" ")
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+for key in list(d.keys()):
+    print(key, ":", d[key])
