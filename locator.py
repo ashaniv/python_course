51b7bc972528c63a1580d0d55fb99e23cd550563
@ashaniv <alon.shin81@gmail.com>
2019-09-16 14:32:43 +0300

    after 'with' and words count_#4

--- locator.py
+++ locator.py
@@ -6,20 +6,18 @@ url = input("Please enter a valid URL:")
 response = requests.get(url)
 #print(response.text)
 
-file = open('response.text', 'w')
-file.write(response.text)
-file.close()
+with open('response.text', 'w') as file:
+    file.write(response.text)
+    d = dict()
 
-d = dict()
-
-for line in file:
-    line = line.strip()
-    line = line.lower()
-    words = line.split(" ")
-    for word in words:
-        if word in d:
-            d[word] = d[word] + 1
-        else:
-            d[word] = 1
-for key in list(d.keys()):
-    print(key, ":", d[key])
+    for line in file:
+        line = line.strip()
+        line = line.lower()
+        words = line.split(" ")
+        for word in words:
+            if word in d:
+                d[word] = d[word] + 1
+            else:
+                d[word] = 1
+    for key in list(d.keys()):
+        print(key, ":", d[key])
